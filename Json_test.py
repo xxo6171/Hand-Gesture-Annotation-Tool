5e802da24bc617ab79221f490b6e65cb357dbbf3
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-06 17:43:15 +0900

    feat: json 기본 형식 정의
    - 기본형식으로 사용할 틀 정의
    - 이 형식으로 output 낼 예정

---
+++ Json_test.py
@@ -0,0 +1,25 @@
+test = {
+    'shapes': [
+        {
+            'label': 'test_label',
+            'points': [
+                [0, 0],
+                [1, 1],
+                [10, 10]
+            ],
+            'shape_type': 'Polygon'
+        },
+        {
+            'label': 'test_label',
+            'points': [
+                [10, 20],
+                [17, 5],
+                [10, 10]
+            ],
+            'shape_type': 'Polygon'
+        }
+    ],
+    'image_path': 'Resource\Image\hand.jpeg',
+    'image_height': 300,
+    'image_width': 300
+}
