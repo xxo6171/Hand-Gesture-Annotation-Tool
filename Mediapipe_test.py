6cd0d2fdf0419280b92afe43cf27fa6823389249
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-06 14:36:25 +0900

    Feat: 이미지 한장만 불러오기
    - 캠으로 실시간 가져오기에서 이미지 한장으로 변경

--- Mediapipe_test.py
+++ Mediapipe_test.py
@@ -11,31 +11,28 @@ with mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
  
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            continue
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
- 
-        results = hands.process(image)
- 
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
- 
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                finger1 = int(hand_landmarks.landmark[4].x * 100 )
-                finger2 = int(hand_landmarks.landmark[8].x * 100 )
-                dist = abs(finger1 - finger2)
-                cv2.putText(
-                    image, text='f1=%d f2=%d dist=%d ' % (finger1,finger2,dist), org=(10, 30),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
-                    color=255, thickness=3)
- 
-                mp_drawing.draw_landmarks(
-                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
- 
-        cv2.imshow('image', image)
-        if cv2.waitKey(1) == ord('q'):
-            break
+    filename = 'Resource\Image\hand.jpeg'
+    image = cv2.imread(filename, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+    results = hands.process(image)
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            finger1 = int(hand_landmarks.landmark[4].x * 100 )
+            finger2 = int(hand_landmarks.landmark[8].x * 100 )
+            dist = abs(finger1 - finger2)
+            cv2.putText(
+                image, text='f1=%d f2=%d dist=%d ' % (finger1,finger2,dist), org=(10, 30),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
+                color=255, thickness=3)
+
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('image', image)
+    cv2.waitKey()
  
 cap.release()
