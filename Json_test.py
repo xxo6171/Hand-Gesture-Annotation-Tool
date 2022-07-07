cd86be995dce47b06edeba49a8c08dd9d673fd44
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-07 10:30:41 +0900

    Feat: create code level dictionary
    - 코드로 딕셔너리 형태 데이터 만들기 구현
    - mediapipe에 적용 테스트 예정

--- Json_test.py
+++ Json_test.py
@@ -1,35 +1,38 @@
 import json
-
+'''
 test = {
     'shapes': [
         {
             'label': 'test_label',
             'points': [
-                [0, 0],
-                [1, 1],
-                [10, 10]
+                [10, 30]
             ],
-            'shape_type': 'Polygon'
-        },
-        {
-            'label': 'test_label',
-            'points': [
-                [10, 20],
-                [17, 5],
-                [10, 10]
-            ],
-            'shape_type': 'Polygon'
+            'shape_type': 'Line'
         }
     ],
     'image_path': 'Resource\Image\hand.jpeg',
     'image_height': 300,
     'image_width': 300
 }
+'''
+'''
+Jason Open
+with open('Resource\json\sample_without_image_data.json', 'r') as f:
+    json_data = json.load(f)
 
-print(test.keys())
+print(json.dumps(json_data, indent='\t'))
+'''
 
-# Jason Open
-# with open('Resource\json\sample_without_image_data.json', 'r') as f:
-#     json_data = json.load(f)
+test_dict = {}
+test_dict['shapes'] = []
+test_dict['image_path'] = 'Resource\Image\hand.jpeg'
+test_dict['image_height'] = 300
+test_dict['image_width'] = 300
 
-# print(json.dumps(json_data, indent='\t'))
+sub_dict = {}
+sub_dict['label'] = 'test_label'
+sub_dict['points'] = []
+sub_dict['points'].append([10, 30])
+sub_dict['shape_type'] = 'Line'
+test_dict['shapes'].append(sub_dict)
+
