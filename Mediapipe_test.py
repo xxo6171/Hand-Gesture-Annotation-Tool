15ede78b0565345fd7c31e589fd477e5271a7acf
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-07 11:04:42 +0900

    Feat: 좌표 정보 입력 기능 추가
    - 좌표 정보룰 Dictionary 형태로 추출

--- Mediapipe_test.py
+++ Mediapipe_test.py
@@ -1,10 +1,13 @@
+from operator import index
 import cv2
 from cv2 import KeyPoint
 import mediapipe as mp
 
+# 제스쳐 포인트 인식 및 출력
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-  
+
+index_pos = 0
 with mp_hands.Hands(
     max_num_hands=1,
     min_detection_confidence=0.5,
@@ -12,7 +15,6 @@ with mp_hands.Hands(
  
     filename = 'Resource\Image\hand.jpeg'
     image = cv2.imread(filename, cv2.IMREAD_COLOR)
-    # image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     results = hands.process(image)
@@ -26,7 +28,43 @@ with mp_hands.Hands(
 
     cv2.imshow('image', image)
     cv2.waitKey()
-    
+
+
+# 포인트 별 레이블 이름 리스트
+pos_mapping_list = [
+                        'Wrist', 
+                        'Thumb_CMC', 'Thumb_MCP','Thumb_IP', 'Thumb_TIP', 
+                        'Index_Finger_MCP', 'Index_Finger_PIP', 'Index_Finger_DIP', 'Index_Finger_TIP', 
+                        'Middle_Finger_MCP', 'Middle_Finger_PIP', 'Middle_Finger_DIP', 'Middle_Finger_TIP', 
+                        'Ring_Finger_MCP', 'Ring_Finger_PIP', 'Ring_Finger_DIP', 'Ring_Finger_TIP',
+                        'Pinky_MCP', 'Pinky_PIP', 'Pinky_DIP', 'Pinky_TIP'
+                    ]
+
+# 기본 구조 생성
+h, w, c = image.shape
+
+test_dict = {}
+test_dict['shapes'] = []
+test_dict['image_path'] = 'Resource\Image\hand.jpeg'
+test_dict['image_height'] = h
+test_dict['image_width'] = w
+
+sub_dict = {}
+sub_dict['label'] = 'Hand Gesture'
+sub_dict['points'] = []
+sub_dict['shape_type'] = 'Gesture Poligon'
+
+test_dict['shapes'].append(sub_dict)
+
+# 좌표 정보 입력
+index_pos = 0
+for data_point in hand_landmarks.landmark:
+    sub_point = {}
+    sub_point[pos_mapping_list[index_pos]] = [int(data_point.x*w), int(data_point.y*h)]
+    sub_dict['points'].append(sub_point)
+    index_pos = index_pos + 1
+
+'''
 h, w, c = image.shape
 
 keypoints = []
@@ -37,3 +75,4 @@ for data_point in hand_landmarks.landmark:
                         #  'Z': data_point.z,         # z축에서의 좌표_정규화됨_손목을 기준으로 카메라에 가까울수록 값이 작다.
                         #   'Visibility': data_point.visibility,
                          })
+'''
