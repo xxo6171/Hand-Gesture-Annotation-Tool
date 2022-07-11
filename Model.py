bd787ab4973f42e8151cacfa26de1ff26d47cddb
Hello <kyung971112@gmail.com>
2022-07-11 11:26:59 +0900

    init : Model 클래스 정의
    - 이미지 데이터 getter, setter 함수 생성

--- Model.py
+++ Model.py
@@ -1,3 +1,9 @@
 class Model:
     def __init__(self):
-        self.data = None
+        self.imgData = None
+
+    def getImgData(self):
+        return self.imgData
+
+    def setImgData(self, img):
+        self.imgData = img
