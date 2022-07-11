cfb6ef92e2f249d14b3d77de0ae4a461ced6da95
Hello <kyung971112@gmail.com>
2022-07-11 11:37:02 +0900

    fix : 주석 줄 바꿈
    - super().__init__() 위로 바꿈

--- Utils/ImageProc.py
+++ Utils/ImageProc.py
@@ -1,5 +1,12 @@
 import cv2
+import numpy as np
 
 class ImageProc:
     def __init__(self):
         pass
+
+    def loadImgData(self, filepath):
+        img = np.fromfile(filepath, np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
