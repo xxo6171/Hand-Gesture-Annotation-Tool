12f65e7ea28a3a301a66cf1f0c3c60fcf20626d7
Hello <kyung971112@gmail.com>
2022-07-11 11:08:22 +0900

    init : canvas class init 정의
    - widget 초기화
    - Open trigger action : openImage 함수 호출 
    - label_Canvas 가운데 정렬

--- ViewModel/Canvas.py
+++ ViewModel/Canvas.py
@@ -2,8 +2,12 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-class Canvas:
+class Canvas(QMainWindow):
     def __init__(self, view, model):
+        #Initialize
+        self.action_Open = view[0]
+        self.label_Canvas = view[1]
+        self.scrollArea_Canvas = view[2]
         '''
         self.list_listWidget = view[0]
         self.add_btn = view[1]
@@ -11,3 +15,10 @@ class Canvas:
         self.string_lineEdit = view[3]
         '''
         self.model = model
+
+        #Triggered connect
+        self.action_Open.triggered.connect(self.OpenImage)
+
+        #Widget setting
+        self.label_Canvas.setAlignment(Qt.AlignCenter)
+        self.scrollArea_Canvas.setWidget(self.label_Canvas)
