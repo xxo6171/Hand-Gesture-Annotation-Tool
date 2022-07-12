3ee4d3885d83710b7ee2d6ad22144557adb79fbb
kyung-min git <kyung971112@gmail.com>
2022-07-12 14:45:33 +0900

    fix: Rename setCtrlFlag
    - setter 이름 변경

--- Model.py
+++ Model.py
@@ -50,7 +50,7 @@ class Model:
 
     def getCtrlFlag(self):
         return self.ctrl_flag
-    def setDrawFlag(self, flag):
+    def getCtrlFlag(self, flag):
         self.ctrl_flag = flag
 
     def getPrePos(self):
