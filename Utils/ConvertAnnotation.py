0548ec67a4415cf86e188ec58edc70241ffe8212
kyungmin-git <kyung971112@gmail.com>
2022-07-14 16:01:39 +0900

    feat : json2Dict 함수
    - json을 읽어서 dict 형태로 return

--- Utils/ConvertAnnotation.py
+++ Utils/ConvertAnnotation.py
@@ -5,5 +5,7 @@ def dict2Json(dict, filepath) :
         json.dump(dict, f, indent='\t')
     print(filepath)
 
-def json2Dict(filename) :
-    pass
+def json2Dict(filepath) :
+    with open(filepath) as f:
+        dict = json.load(f)
+    return dict
