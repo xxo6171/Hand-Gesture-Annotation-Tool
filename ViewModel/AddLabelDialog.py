a1b7cf887c3497eb7b03995d28aff74b5c3a3bdd
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-14 10:38:00 +0900

    feat: add label dialog
    - 그리기 끝날 시 라벨 추가 다이얼로그
    - Model 안에 모든 정보 저장

---
+++ ViewModel/AddLabelDialog.py
@@ -0,0 +1,45 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from numpy import str0
+import Model
+
+add_label_UI_dir = 'Resource/UI/Add Lable Dialog.ui'
+add_label_form_class = uic.loadUiType(add_label_UI_dir)[0]
+
+class AddLabelDialog(QDialog, add_label_form_class):
+    def __init__(self, model):
+        super().__init__()
+        self.setupUi(self)
+
+        self.model = model
+        self.initListWidget()
+
+        self.listWidget_LabelList.itemClicked.connect(self.clickItem)
+        self.listWidget_LabelList.itemDoubleClicked.connect(self.doubleClickItem)
+
+        self.pushButton_OK.clicked.connect(self.setLabel)
+        self.pushButton_Cancel.clicked.connect(self.closeDialog)
+        
+    def initListWidget(self):
+        self.label_list = self.model.getLabelList()
+        for label in self.label_list:
+            add_label = QListWidgetItem(label)
+            self.listWidget_LabelList.addItem(add_label)
+
+    def clickItem(self):
+        label = self.listWidget_LabelList.currentItem().text()
+        self.lineEdit_NewLabel.setText(label)
+
+    def doubleClickItem(self):
+        self.setLabel()
+
+    def setLabel(self): 
+        label_name = self.lineEdit_NewLabel.text()
+        if label_name not in self.label_list:
+            self.model.setLabel(label_name)
+        self.close()
+
+    def closeDialog(self):
+        self.close()
