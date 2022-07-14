dbc267463c6c7b860bd30700b1206f51687bc162
kyungmin-git <kyung971112@gmail.com>
2022-07-14 13:15:27 +0900

    feat : dict2Json 작업
    - 파라미터 filepath를 입력 받아 dict를 json으로 저장

--- Utils/ConvertAnnotation.py
+++ Utils/ConvertAnnotation.py
@@ -1,7 +1,9 @@
 import json
 
-def dict2Json(dict) :
-    pass
+def dict2Json(dict, filepath) :
+    with open(filepath, 'w') as f:
+        json.dump(dict, f, indent='\t')
+    print(filepath)
 
 def json2Dict(filename) :
     pass
