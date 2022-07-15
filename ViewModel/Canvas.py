9acf855886c945d8811f3b6764df5daaa3dffce2
kyungmin-git <kyung971112@gmail.com>
2022-07-15 09:28:56 +0900

    fix: openImage 이름 변경
    - openFile로 변경

--- ViewModel/Canvas.py
+++ ViewModel/Canvas.py
@@ -43,7 +43,7 @@ class Canvas(QWidget):
         self.listWidget_LabelList = view[8]
 
         # Triggered connect
-        self.action_Open.triggered.connect(self.openImage)
+        self.action_Open.triggered.connect(self.openFile)
         self.action_Save.triggered.connect(self.saveJson)
 
         self.action_Polygon.triggered.connect(self.drawPoly)
@@ -78,7 +78,7 @@ class Canvas(QWidget):
             self.menu_Zoom.setEnabled(False)
             self.action_Save.setEnabled(False)
 
-    def openImage(self):
+    def openFile(self):
         self.filePath = QFileDialog.getOpenFileName(self, 'Open File',filter='Images(*.jpg *.jpeg *.png *.json)')
 
         if self.filePath[0] == '' :
