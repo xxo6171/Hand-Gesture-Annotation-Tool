96537f2ce547872003be4d0f92711d03a0d2c305
kyungmin-git <kyung971112@gmail.com>
2022-07-15 09:31:32 +0900

    feat : openFile 단축키 추가
    - ctrl + O 입력 시 openFile() 함수 호출

--- ViewModel/Canvas.py
+++ ViewModel/Canvas.py
@@ -179,6 +179,8 @@ class Canvas(QWidget):
     def keyReleaseEvent(self, event):  # Release Control Key
         if event.key() == Qt.Key_Control:
             self.model.setCtrlFlag(False)
+        if event.key() == Qt.Key_Control and event.key() == Qt.Key_O :
+            self.openFile()
 
     def wheelEvent(self, event):       # Move Mouse Wheel
         if not self.model.getFocusFlag():
