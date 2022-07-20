379471bf4197b303de86c6ec1c342a69a95cd694
kyungmin-git <kyung971112@gmail.com>
2022-07-20 10:17:27 +0900

    fix: loadImgData 함수 수정
    - 코드 간결

--- Utils/ImageProc.py
+++ Utils/ImageProc.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 
 def loadImgData(filepath):
-    img_data = np.fromfile(filepath, np.uint8)
-    img_data = cv2.imdecode(img_data, cv2.IMREAD_COLOR)
-    img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB)
+    img_data = cv2.cvtColor(cv2.imdecode(np.fromfile(filepath, np.uint8), cv2.IMREAD_COLOR), cv2.COLOR_BGR2RGB)
     h,w,c = img_data.shape
     return img_data, w, h, c
 
