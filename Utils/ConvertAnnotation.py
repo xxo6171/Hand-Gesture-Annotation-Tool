0f05555a077316bb84872923a5344f69145f32d2
kyungmin-git <kyung971112@gmail.com>
2022-07-20 12:09:18 +0900

    fix: print 수정
    - print 수정

--- Utils/ConvertAnnotation.py
+++ Utils/ConvertAnnotation.py
@@ -4,7 +4,7 @@ import time
 def dict2Json(dict, filepath) :
     with open(filepath, 'w') as f:
         json.dump(dict, f, indent='\t')
-    print('Saved file\n' + time.strftime('%c', time.localtime()) + ' ' + filepath)
+    print('Saved file\n' + time.strftime('%c', time.localtime()) + '\n' + filepath + '\n')
 
 def json2Dict(filepath) :
     with open(filepath) as f:
