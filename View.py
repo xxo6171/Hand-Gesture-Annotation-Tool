c43f37a4298521ba2fcf284ccd6dfcbd8bf19df4
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-26 10:47:19 +0900

    fix: 소문자 변경

--- View.py
+++ View.py
@@ -1,12 +1,11 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+# from PyQt5.QtCore import *
+# from PyQt5.QtGui import *
 import sys
 
 import Model
 import ViewModel
-import Widgets
 
 mainUI_dir = 'Resource/UI/Main GUI.ui'
 main_form_class = uic.loadUiType(mainUI_dir)[0]
@@ -15,21 +14,21 @@ class HandAnnot(QMainWindow, main_form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        
+
         # Binding
         self.Model = Model.Model()
         self.ViewModel = ViewModel.ViewModel(self.getComponents(), self.Model)
     
     def getComponents(self):
-        Components = []
+        components = []
         
         actions = []
         widgets = []
         
-        Components.append(actions)
-        Components.append(widgets)
+        components.append(actions)
+        components.append(widgets)
 
-        return Components
+        return components
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
