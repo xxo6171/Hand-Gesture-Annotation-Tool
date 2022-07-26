5655aaef89e6364c5b3ad72c27ad3eb23576698f
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-26 11:37:58 +0900

    feat: init zoom

--- Widgets/Zoom.py
+++ Widgets/Zoom.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import *
-# from PyQt5.QtCore import *
+from PyQt5.QtCore import *
 # from PyQt5.QtGui import *
 
 from Widgets.Display import *
@@ -15,3 +15,40 @@ class Zoom(QWidget):
 
         # Init Display Class
         self.Display = Display(self.canvas, self.scroll_area, self.Model)
+
+        # Set Focus Policy
+        self.setFocusPolicy(Qt.ClickFocus)
+
+
+    # ----- Set View -----
+    def setCanvas(self):
+        pass
+    
+
+    # ----- Focus Event -----
+    def focusInEvent(self, event):
+        pass
+
+    def focusOutEvent(self, event):
+        pass
+
+
+    # ----- Key Event -----
+    def keyPressEvent(self, event):
+        pass
+
+    def keyReleaseEvent(self, event):
+        pass
+
+
+    # ----- Wheel Event -----
+    def wheelEvent(self, event):
+        pass
+
+
+    # ----- Zoom -----
+    def ZoomIn(self):
+        pass
+    
+    def ZoomOut(self):
+        pass
