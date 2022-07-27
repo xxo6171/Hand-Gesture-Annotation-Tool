df717442bd35ddcd13b28c6420f81599221441e1
KimJinSeong-Git <acmiheee@gmail.com>
2022-07-27 12:13:09 +0900

    feat: open file
    - 이미지 파일 열기 기능 이식 완료
    - 현재 List Widget 처리 함수는 제외

--- HandAnnot.py
+++ HandAnnot.py
@@ -2,12 +2,16 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import *
 # from PyQt5.QtCore import *
 # from PyQt5.QtGui import *
-import sys
 
+import sys
+import os
 
 from Model import *
 from Widgets.Draw import *
 from Widgets.Zoom import *
+
+from Utils.ConvertAnnotation import *
+from Utils.ImageProc import *
 
 mainUI_dir = 'Resource/UI/Main GUI.ui'
 main_form_class = uic.loadUiType(mainUI_dir)[0]
@@ -48,7 +52,78 @@ class HandAnnot(QMainWindow, main_form_class):
 
     # ----- File Actions -----
     def openFile(self):
-        pass
+        # File Open by Dialog
+        title = 'Open Image'
+        filter = 'Images(*.jpg *.jpeg *.png)'
+        file_path = QFileDialog.getOpenFileName(self, title, filter=filter)[0]
+
+        # Exception Handling - Unavailable File Path
+        if file_path == '': 
+            return
+
+        # Extract Image File Name
+        base_name = os.path.basename(file_path)
+        file_name, extension_type = os.path.splitext(base_name)
+
+        # Set *.json File Name for Exist Check
+        json_path = os.path.dirname(file_path) + '/' + file_name + '.json'
+
+        '''!!!! 여기 과연 필요할까 !!!!'''
+        self.initWindow()
+
+        # Load Image From File Path
+        img, w, h, c = loadImgData(file_path)
+
+        if ( extension_type == '.json' ) or ( os.path.isfile(json_path) ):
+            # Save Loaded Annotation Info to Model
+            opened_annot_info = json2Dict(json_path)
+            self.Model.setAnnotDict(opened_annot_info)
+
+            '''!!! 정규화 - 많이 쓰이기 때문에 함수화 팔요 !!!'''
+            cur_annot_info = self.Model.getAnnotInfo(no_deep = True)
+            shapes = cur_annot_info['shapes']
+            for shape in shapes:
+                points = shape['points']
+                for point in points:
+                    point[0] /= w
+                    point[1] /= h
+        else :
+            self.Model.setAnnotInfo(file_path, w, h)
+
+        # Save Original Image to Model
+        self.Model.setImgData(img, w, h, c)
+
+        # Save Image for Display to Model
+        img_QPixmap = self.img2QPixmap(img, w, h, c)
+        self.Model.setImgScaled(img_QPixmap, w, h, c)
+
+        # Activate Menu
+        self.Model.setMenuFlag(True)
+        self.menuRefresh()
+
+    def menuRefresh(self):
+        if self.Model.getMenuFlag():
+            self.menu_Edit.setEnabled(True)
+            self.menu_Zoom.setEnabled(True)
+            self.action_Save.setEnabled(True)
+        else:
+            self.menu_Edit.setEnabled(False)
+            self.menu_Zoom.setEnabled(False)
+            self.action_Save.setEnabled(False)
+
+    ''' 없애는 방법 생각해야함'''
+    def initWindow(self):
+        self.Model.setImgData(None, None, None, None)
+        self.Model.setImgScaled(None, None, None, None)
+        self.Model.initAnnotStack()
+        self.Model.initAnnotInfo()
+        self.Model.initLabelList()
+
+    def img2QPixmap(self, img, w, h, c):
+        qImg = QImage(img.data, w, h, w * c, QImage.Format_RGB888)
+        qPixmap = QPixmap.fromImage(qImg)
+
+        return qPixmap
 
     def saveJson(self):
         pass
