9f7b52a6f9372f4a12a1dfdb7dbf8c0a6c308f4a
kyungmin-git <kyung971112@gmail.com>
2022-08-01 14:58:00 +0900

    fix: menuRefresh 변경
    - TF 변수 필요 없음

--- HandAnnot.py
+++ HandAnnot.py
@@ -145,12 +145,10 @@ class HandAnnot(QMainWindow, main_form_class):
             self.listWidget_ObjectList.addItem(QListWidgetItem(obj_type + '_' + obj_label))
 
     def menuRefresh(self):
-        # 메뉴 플래그가 True면 TF=True, False면 TF=False
-        TF = True if self.Model.getMenuFlag() else False
         # 메뉴 아이템 배열에 저장
         mItems = [self.menu_Edit, self.menu_Zoom, self.action_Save]
         for mItem in mItems:
-            mItem.setEnabled(TF)
+            mItem.setEnabled(self.Model.getMenuFlag())
 
     def img2QPixmap(self, img, w, h, c):
         qImg = QImage(img.data, w, h, w * c, QImage.Format_RGB888)
